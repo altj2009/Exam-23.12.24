76f5fce0f28d1fc75a16ad3f3ee28f9492c0f84e
altysh <altysh@gmail.com>
2024-12-23 23:44:21 +0600

    вот код

--- src/Accountant.java
+++ src/Accountant.java
@@ -1,4 +1,118 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Accountant {
+    public Employee[] employees;
+    private int counter;
+
+    public Accountant() {
+        this.employees = new Employee[0]; // Изначально массив пустой
+        this.counter = 0;
+    }
+
+    Scanner scan = new Scanner(System.in);
+
+    public void addEmployee(Employee employee) {
+        Employee[] newEmployees = new Employee[counter + 1];
+        for (int i = 0; i < counter; i++) {
+            newEmployees[i] = employees[i];
+        }
+        newEmployees[counter] = employee;
+        employees = newEmployees;
+        counter++;
+    }
+
+    public void performAction(){
+        Accountant buhgalter = new Accountant();
+        while (true){
+            buhgalter.getMenu();
+            int choice = scan.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Enter the name of the employee: ");
+                    String name = scan.next();
+                    System.out.println("Enter the base salary: ");
+                    int baseSalary = scan.nextInt();
+                    System.out.println("Enter the hourly rate: ");
+                    int hourlyRate = scan.nextInt();
+                    if(hourlyRate<0){
+                        System.out.println("Error! hourly rate can't be minus!");
+                    }
+                    System.out.println("Enter the worked hours: ");
+                    int workedHours = scan.nextInt();
+                    if(workedHours<0){
+                        System.out.println("Error! hours can't be minus!");
+                    }
+                    scan.nextLine();
+
+                    Employee employee1 = new Employee(baseSalary, hourlyRate, name, workedHours);
+                    buhgalter.addEmployee(employee1);
+                    break;
+                case 2:
+                    buhgalter.calculatePayroll();
+                    break;
+                case 3:
+                    for(int i = 0; i < buhgalter.employees.length ; i++){
+                        System.out.println(buhgalter.employees[i].toString());
+                    }
+                    break;
+                case 4:
+                    System.out.println("choose the operation: 1(+) | 2(-) | 3(*) | 4(/) ");
+                    int choose = scan.nextInt();
+                    if(choose<0 || choose>4){
+                        System.out.println("ur choice of operation is uncorrect!");
+                        break;
+                    }
+                    System.out.println("enter the first number: ");
+                    int number1 = scan.nextInt();
+                    System.out.println("enter the second number: ");
+                    int number2 = scan.nextInt();
+                    switch (choose){
+                        case 1:
+                            System.out.println("result : "+ (number1+number2));
+                            break;
+                        case 2:
+                            System.out.println("result : " + (number1-number2));
+                            break;
+                        case 3:
+                            System.out.println("result : "+ number1*number2);
+                            break;
+                        case 4:
+                            System.out.println("result : "+ number1/number2);
+                            break;
+                    }
+                    break;
+                case 0:
+                    System.out.println("u succesly exit the sistem!");
+                    return;
+            }
+
+        }
+
+    }
+
+    public void calculatePayroll(){
+        for(int i = 0; i < employees.length ; i++) {
+            System.out.println((employees[i].getHourlyRate() + " * " + employees[i].getWorkedHours()) + " + " + employees[i].getBaseSalary());
+            System.out.println("Total salary of  ~"+ employees[i].getName() + "~  equals : " + employees[i].calculateSalary());
+        }
+    }
+
+    public Employee[] getEmployes() {
+        return employees;
+    }
+
+    public void setEmployees(Employee[] employees) {
+        this.employees = employees;
+    }
+
+    public void getMenu(){
+        System.out.println("""
+                Выберите действие:
+                1. Добавить нового сотрудника
+                2. Рассчитать зарплату для всех сотрудников
+                3. Показать информацию о сотрудниках
+                4. Калькулятор (для дополнительных расчетов)
+                0. Выход
+                """);
+    }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
-
 public class Employee {
+    private String name;
+    private int baseSalary;
+    private int workedHours;
+    private int hourlyRate;
+
+    public Employee(int baseSalary, int hourlyRate, String name, int workedHours) {
+        this.baseSalary = baseSalary;
+        this.hourlyRate = hourlyRate;
+        this.name = name;
+        this.workedHours = workedHours;
+    }
+
+    public int calculateSalary(){
+        int total = (workedHours*hourlyRate)+baseSalary;
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee |" +
+                "baseSalary:" + baseSalary +
+                ", name:'" + name + '\'' +
+                ", workedHours:" + workedHours +
+                ", hourlyRate:" + hourlyRate +
+                '|';
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public  void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWorkedHours() {
+        return workedHours;
+    }
+
+    public void setWorkedHours(int workedHours) {
+        this.workedHours = workedHours;
+    }
 }
